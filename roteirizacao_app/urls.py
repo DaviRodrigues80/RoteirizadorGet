05e30b0de12b9d2567c457b7a64739494c6dc683
Davi Rodrigues <davirodrigues_sp@hotmail.com>
2024-03-03 05:53:10 -0300

    versão v1.0 - atualização e configuração para deployer 4

--- roteirizacao_app/urls.py
+++ roteirizacao_app/urls.py
@@ -1,23 +1,10 @@
-from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
-from .views import EscolherPlanoView, CustomUserCreationView, atualizar_creditos,  autoriza_criar_rota, buscar_pagamentos, editar_pagamento, enviar_email, excluir_pagamento, lista_enderecos, adicionar_endereco, home, lista_pagamento, pagamento_form, verificar_saldo
+from django.contrib import admin
+from django.urls import path, include
+from roteirizacao.views import signup
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('signup/', CustomUserCreationView.as_view(), name='signup'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('lista_enderecos', lista_enderecos, name='lista_enderecos'),
-    path('adicionar/', adicionar_endereco, name='adicionar'),
-    path('escolher_plano/', EscolherPlanoView.as_view(), name='escolher_plano'),
-    path('enviar_email/', enviar_email, name='enviar_email'),
-    path('verificar_saldo/', verificar_saldo, name='verificar_saldo'),
-    path('lista_pagamento/', lista_pagamento, name='lista_pagamento'),
-    path('pagamento/', pagamento_form, name='pagamento'),
-    path('autoriza_criar_rota/', autoriza_criar_rota, name='autoriza_criar_rota'),
-    path('buscar_pagamentos/', buscar_pagamentos, name='buscar_pagamentos'),
-    path('editar_pagamento/<int:pagamento_id>/', editar_pagamento, name='editar_pagamento'),
-    path('excluir_pagamento/<int:pagamento_id>/', excluir_pagamento, name='excluir_pagamento'),
-    path('atualizar_creditos/', atualizar_creditos, name='atualizar_creditos'),
-
+    path('admin/', admin.site.urls),
+    path('', include('roteirizacao_app.urls')),
+    path('signup/', signup, name='signup'),
 ]
+
